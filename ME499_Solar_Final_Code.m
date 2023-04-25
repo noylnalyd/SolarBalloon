1139e606ddcff6864d9ea8bc1bd78119e1f1c684
ajfeng1 <123188850+ajfeng1@users.noreply.github.com>
2023-04-25 16:41:55 -0400

    Add files via upload

--- ME499_Solar_Final_Code.m
+++ ME499_Solar_Final_Code.m
@@ -12,11 +12,29 @@ g = 9.776;
 M = 2.016;
 R = 0.0821;
 
+% H2 density
 rho_H2 = (P_atm/101300)*M/(R*(T_atm + 273))
 
+% Total mass that can be lifted
 r = 15;
-m = rho_atm*((4/3)*pi*r^3) - (rho_H2)*((4/3)*pi*r^3)
+m_lift = rho_atm*((4/3)*pi*r^3) - (rho_H2)*((4/3)*pi*r^3)
 
+% Cable mass
+linear_density = (31.43/1000)*(3280.84)*(1/2.2);
+L_cable = 10 %km;
+m_cable = linear_density*L_cable
+
+% Balloon mass
+rho_balloon = 916; %kg/m3
+A_balloon = 4*pi*r^2;
+thickness_balloon = 0.002/100 %m;
+m_balloon = rho_balloon*A_balloon*thickness_balloon;
+
+% Solar panel mass
+m_payload = m_lift - m_cable - m_balloon;
+SF = 1.2;
+m_panel = m_payload/1.2
+A_panel = m_panel/11.66
 
 
 %% NASA study
@@ -44,7 +62,8 @@ x = Data001(:,1);
 y = Data001(:,2);
 plot(x,y);
 
-
+xlabel('Altitude (km)','FontSize', 22);
+ylabel('Irradiance (W/(m^2)','FontSize', 22);
 
 
 
